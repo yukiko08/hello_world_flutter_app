c8a6ef19809cc7d984ebbc86e7aea8b65cc62a9f
吉川裕木子 <yukiko19960829@yahoo.co.jp>
2020-01-23 14:01:06 +0900

    1/23 stady 2h

--- lib/widget/local_notification_widget.dart
+++ lib/widget/local_notification_widget.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:hello_world_flutter_app/page/second_page.dart';
 
 class LocalNotificationWidget extends StatefulWidget{
   @override
-  _LocalNotificationWidgetState createState() => _LocalNotificationWidgetState();
+  _LocalNotificationWidgetState createState() =>
+      _LocalNotificationWidgetState();
 
 }
 
 class _LocalNotificationWidgetState extends State<LocalNotificationWidget> {
+  final notification = FlutterLocalNotificationsPlugin();
+
   @override
   void initState(){
     super.initState();
@@ -24,11 +28,34 @@ class _LocalNotificationWidgetState extends State<LocalNotificationWidget> {
 
   Future onSelectNotification(String payload) async => await Navigator.push(context,
 
-  MaterialPageRoute(builder: (context => SecondPage(payload: payload))),
+  MaterialPageRoute(builder: (context) => SecondPage(payload: payload)),
   );
 
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) => Padding(
+    padding: const EdgeInsets.all(8.0),
+    child:ListView(
+      children: <Widget>[
+        title('Basics'),
+        RaisedButton(
+          child: Text('show notification'),
+          onPressed: () => showOngoingNotification(notifications,
+          title: 'Title', body: 'Body'),
+        ),
+      ],
+    )
+
+  );
+
+  Widget title(String text) => Container(
+    margin: EdgeInsets.symmetric(vertical: 4),
+    child: Text(
+      text,
+      style: Theme.of(context).textTheme.title,
+      textAlign: TextAlign.center,
+    ),
+  );
+
 }
 
 
