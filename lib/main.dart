e035f84791b181a8c1c0adb5cf1bcb021eadf0e6
吉川裕木子 <yukiko19960829@yahoo.co.jp>
2020-02-20 21:53:24 +0900

    初期化２

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'Chutorial Flutter',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Chutorial Flutter'),
+          ),
+          body: Center(
+            child: Text('Hello World'),
+          ),
+
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -92,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'ボタンを押した回数:',
             ),
             Text(
               '$_counter',
